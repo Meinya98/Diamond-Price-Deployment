d20dca61e2e19ace2e265c2f79ffc2ae14f24abc
Rafli Maulana <127012237+Meinya98@users.noreply.github.com>
2025-06-05 16:22:13 +0700

    Add files via upload

--- pages/home.py
+++ pages/home.py
@@ -1,17 +1,18 @@
 import streamlit as st
+import streamlit.components.v1 as stc
 from styles.custom_css import inject_css
 
 def show():
     inject_css()
-    st.markdown("""
+    html_temp="""
     <div class="header-box">
         <h1 style="color: #2c3e50; text-align: center; margin-bottom: 5px;">Diamond Price Prediction</h1>
         <p style="color: #6c757d; text-align: center; font-size: 16px;">
             Estimate the market value of your diamond with precision
         </p>
     </div>
-    """, unsafe_allow_html=True)
-    st.markdown("""
+    """
+    desc_temp="""
     <div style="background-color: #f8f9fa; border-radius: 10px; padding: 20px; margin-bottom: 20px;">
         <h3 style="color: #2c3e50;">About This App</h3>
         <p style="color: #495057;">
@@ -40,4 +41,6 @@ def show():
             </a>
         </p>
     </div>
-    """, unsafe_allow_html=True)
+    """
+    stc.html(html_temp)
+    st.markdown(desc_temp, unsafe_allow_html=True)
